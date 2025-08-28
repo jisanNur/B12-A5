1439945dc119c516d66ba057613ac5f074a4a8c9
Jisanur <jisanur888.jr@gmail.com>
2025-08-28 23:41:28 +0600

    call history clear btn added done

--- js/script.js
+++ js/script.js
@@ -1,36 +1,61 @@
 
-function getIdElement(id){
+function getIdElement(id) {
     const element = document.getElementById(id);
     return element;
 }
 
-getIdElement("cards-btns").addEventListener("click", function (e){
+// call button functionality 
+getIdElement("cards-btns").addEventListener("click", function (e) {
 
 
-    if(e.target.className.includes("call-btn")){
+    if (e.target.className.includes("call-btn")) {
         const callBtn = e.target;
         const coinBtn = callBtn.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.children[0].children[1].children[1].children[0].innerText;
         const heatBtn = callBtn.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.children[0].children[1].children[0].children[0].innerText;
-        const copyBtn = callBtn.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.children[0].children[1].children[0].innerText;
         const cardTitle = callBtn.parentNode.parentNode.children[1].children[0].innerText;
         const serviceNumber = callBtn.parentNode.parentNode.children[1].children[3].children[0].innerText
-        console.log(serviceNumber)
+        const currenTime = new Date()
+
+        console.log(currenTime)
+        
+        
 
         const currentCoin = Number(coinBtn);
-        
-        if(currentCoin < 20){
+
+        if (currentCoin < 20) {
             alert("❌ You don't have enough coins! You need at least 20 coins to make a call.")
             return;
         }
         let newCoin = currentCoin - 20;
-        
+
         alert(`📞 Calling ${cardTitle} ${serviceNumber}`)
 
         getIdElement("coin-icon").innerText = newCoin;
 
         
+        const callHistory = document.createElement("div");
+        callHistory.innerHTML = `
+            <div class="w-full bg-gray-100 p-3 rounded-lg flex justify-between items-center">
+                        <div>
+                            <h3 class="font-bold">${cardTitle}</h3>
+                            <p>${serviceNumber}</p>
+                        </div>
 
-        
+                        <p>${currenTime.toLocaleTimeString()}</p>
+                    </div>
+        `
+
+        getIdElement("call-history").append(callHistory);
+  
     }
-    
+
 })
+
+// call history functionality
+getIdElement("clear-btn").addEventListener("click", function(){
+    getIdElement("call-history").innerHTML = '';
+})
+
+// copy button functionality
+getIdElement("")
+
